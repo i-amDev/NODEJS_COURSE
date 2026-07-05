baeae85a36a308f220009b5d9697a04abc8e58d7
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-07-05 20:35:18 +0530

    - Learned about "request" object. - Learned how to send responses back to the client.

--- FIrstNodeServer/app.js
+++ FIrstNodeServer/app.js
@@ -9,7 +9,13 @@ const http = require("http");
 // });
 
 http.createServer((req, res) => {
-    console.log(req.url);
+    console.log(req.url, req.method, req.headers);
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Hello World</title></head>");
+    res.write("<body><h1>Welcome to NodeJS</h1></body>");
+    res.write("</html>");
+    res.end();
 }).listen(3000, () => {
     console.log("Server started on port 3000");
 });
