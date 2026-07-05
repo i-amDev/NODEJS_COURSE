a4450a20c9e7881578e219331708fc6288259bd7
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-07-05 17:32:02 +0530

    - Getting started with NodeJS. - Imported "fs" to read and write into files. - Create a file with some text output.

--- first.js
+++ first.js
@@ -1,1 +1,8 @@
+const fs = require("fs");
+
 console.log("First JS File.");
+
+fs.writeFile("output.txt", "SOME TEXT", (err) => {
+    if (err) console.log("Error creating output.txt");
+    else console.log("Successfully created output.txt");
+});
