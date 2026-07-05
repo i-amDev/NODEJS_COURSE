1a2da0068a3517da01dfb1399472b5d3f250c583
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-07-05 19:06:40 +0530

    - Refactored. - Another way of creating NodeJs server.

--- FIrstNodeServer/app.js
+++ FIrstNodeServer/app.js
@@ -1,9 +1,15 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-    console.log(req);
-});
+// const server = http.createServer((req, res) => {
+//     console.log(req);
+// });
+//
+// server.listen(3000, () => {
+//     console.log("Server started on port 3000");
+// });
 
-server.listen(3000, () => {
+http.createServer((req, res) => {
+    console.log(req.url);
+}).listen(3000, () => {
     console.log("Server started on port 3000");
 });
