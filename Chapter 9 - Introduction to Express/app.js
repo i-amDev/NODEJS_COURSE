46d0ac070afbd1c3b75ed271b70efd20af7dcc12
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-07-06 11:36:14 +0530

    - Added two "middlewares".

--- Chapter 9 - Introduction to Express/app.js
+++ Chapter 9 - Introduction to Express/app.js
@@ -9,6 +9,17 @@ const requestHandler = require("./user");
 
 const app = express();
 
+// Adding middleware
+app.use((req,res,next) => {
+    console.log("First middleware", req.url, req.method);
+    next();
+});
+
+app.use((req,res,next) => {
+    console.log("Second middleware", req.url, req.method);
+    res.send("<h1>Hello from middleware!</h1>");
+});
+
 const server = http.createServer(app);
 
 server.listen(3000, () => {
