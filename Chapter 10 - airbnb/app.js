24614ea7acc87c266f9d7dd09093cae330452286
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-07-06 15:53:20 +0530

    - Extracted the common path from the hostRouter and kept it at a single place in "app.js".

--- Chapter 10 - airbnb/app.js
+++ Chapter 10 - airbnb/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.urlencoded());
 
 app.use(userRouter);
-app.use(hostRouter);
+app.use("/host", hostRouter);
 
 app.use((req, res, next) => {
     res.status(404).send("<h1>Not Found</h1>");
