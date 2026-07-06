1ffb8845d1d6867527c675b1d3f0531d91b89968
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-07-06 05:58:10 +0530

    - Created a form to take user inputs. - Learned about reading chunks.

--- FIrstNodeServer/app.js
+++ FIrstNodeServer/app.js
@@ -10,7 +10,7 @@ const fs = require("fs");
 // });
 
 http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    console.log(req.url, req.method);
     if (req.url === "/home") {
         res.setHeader("Content-Type", "text/html");
         res.write("<html>");
@@ -32,6 +32,30 @@ http.createServer((req, res) => {
         res.statusCode = 302;
         res.setHeader("Location", "/home");
         return res.end();
+    }
+    else if (req.url === '/data') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Complete Coding</title></head>');
+        res.write('<body><h1>Enter Your Details:</h1>');
+        res.write('<form action="/submit-details" method="POST">');
+        res.write('<input type="text" name="username" placeholder="Enter your name"><br>');
+        res.write('<label for="male">Male</label>')
+        res.write('<input type="radio" id="male" name="gender" value="male" />')
+        res.write('<label for="female">Female</label>')
+        res.write('<input type="radio" id="female" name="gender" value="female" />')
+        res.write('<br><input type="submit" value="Submit">');
+        res.write('</form>');
+        res.write('</body>');
+        res.write('</html>');
+        return res.end();
+    }
+    else if (req.url.toLowerCase() === "/submit-details" && req.method === "POST") {
+        req.on("data", (chunk) => {
+            console.log(chunk);
+        });
+        res.statusCode = 302;
+        res.setHeader('Location', '/data');
     }
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
