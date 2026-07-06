0a9614e008a07469b0f25cb89b97b0b7321eb0cc
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-07-06 15:50:30 +0530

    - Added the "404 Not Found" response.

--- Chapter 10 - airbnb/app.js
+++ Chapter 10 - airbnb/app.js
@@ -12,6 +12,10 @@ app.use(express.urlencoded());
 app.use(userRouter);
 app.use(hostRouter);
 
+app.use((req, res, next) => {
+    res.status(404).send("<h1>Not Found</h1>");
+});
+
 app.listen(3000, () => {
     console.log("Server started on port 3000");
 });
