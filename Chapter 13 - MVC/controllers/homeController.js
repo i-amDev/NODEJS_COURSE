353de90f60cd4f7fd350ed89573772b69813a150
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-07-07 07:14:59 +0530

    - Created a "homeModel" model class. - Also created a "save" method to save the data, and also created a static function "fetchAll" to fetch the data.

--- Chapter 13 - MVC/controllers/homeController.js
+++ Chapter 13 - MVC/controllers/homeController.js
@@ -1,17 +1,21 @@
 const path = require("path");
 
+const Home = require("../models/homeModel");
+
 exports.getAddHome = (req,res,next) => {
     res.sendFile(path.join(__dirname, "../", "views", "addHome.html"));
 };
 
-const registeredHomes = [];
 exports.postAddHome = (req,res,next) => {
     console.log("Home registration successful for : ", req.body, req.body.houseName);
-    registeredHomes.push({houseName : req.body.houseName});
+    const home = new Home(req.body.houseName, 999, "Pune", 5, "asdf");
+    // registeredHomes.push({houseName : req.body.houseName});
+    home.save();
     res.sendFile(path.join(__dirname, "../", "views", "homeAdded.html"));
 };
 
 exports.getHomes = (req,res,next) => {
+    const registeredHomes = Home.fetchAll();
     console.log(req.url, req.method);
     console.log(registeredHomes);
     // res.sendFile(path.join(__dirname, "../", "views", "home.ejs"));
