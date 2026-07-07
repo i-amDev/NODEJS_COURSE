bacef10dc7e5979a965aee8ecbf8388a3693ea53
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-07-07 11:11:09 +0530

    - Added a new route for fetching the "homeId" from the UI.

--- Practise Set/Chapter 13 - MVC/controllers/storeController.js
+++ Practise Set/Chapter 13 - MVC/controllers/storeController.js
@@ -36,3 +36,12 @@ exports.getFavouriteList = (req, res, next) => {
     })
   );
 };
+
+exports.getHomeDetails = (req, res, next) => {
+    const homeId = req.params.homeId;
+    console.log(homeId);
+    res.render("store/home-detail", {
+        pageTitle: "Home Details",
+        currentPage: "Home",
+    });
+};
