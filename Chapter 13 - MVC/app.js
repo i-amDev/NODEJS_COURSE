d5d2c81c5fd33f84767da1a492088c6a886d058f
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-07-07 06:54:35 +0530

    - Created a separate "errorController" for handling the error related functionalities.

--- Chapter 13 - MVC/app.js
+++ Chapter 13 - MVC/app.js
@@ -1,12 +1,10 @@
-// Core Modules
-const path = require("path");
-
 // External Modules
 const express = require("express");
 
 // Local Modules
 const userRouter = require("./routes/userRouter");
 const {hostRouter}  = require("./routes/hostRouter");
+const errorController = require("./controllers/errorController");
 
 const app = express();
 
@@ -18,9 +16,7 @@ app.use(express.urlencoded());
 app.use(userRouter);
 app.use("/host", hostRouter);
 
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-});
+app.use(errorController.pageNotFound);
 
 app.listen(3000, () => {
     console.log("Server started on port 3000...");
