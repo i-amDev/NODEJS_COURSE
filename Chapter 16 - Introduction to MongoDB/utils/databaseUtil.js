c2d09f8073dd19d0b7d713cf7b0286f6313f2f16
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-07-09 11:02:02 +0530

    - Added the configuration required to connect the application to "mongoDB".

--- Chapter 16 - Introduction to MongoDB/utils/databaseUtil.js
+++ Chapter 16 - Introduction to MongoDB/utils/databaseUtil.js
@@ -1,10 +1,24 @@
-const mysql = require('mysql2');
+const mongo = require('mongodb');
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'Completecoding@01',
-  database: 'airbnb'
-});
+const MongoClient = mongo.MongoClient;
 
-module.exports = pool.promise();
+const MONGO_URL = "mongodb://127.0.0.1:27017/airbnb";
+
+let _db; // _ usually mean private member
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(MONGO_URL).then(client => {
+      callback();
+      _db = client.db("airbnb");
+  }).catch(error => console.log("Error while connecting to mongo" ,error));
+};
+
+const getDB = () => {
+    if (!_db) {
+        throw new Error("MongoDB connection failed/not connected");
+    }
+    return _db;
+}
+
+module.exports = mongoConnect;
+module.getDB = getDB;
