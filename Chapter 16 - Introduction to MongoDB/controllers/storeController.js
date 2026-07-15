7a6e6930c34e234bea7f738014e5c64e48ad48e5
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-07-15 17:25:33 +0530

    - Implemented the "save" method. - Used the "save" method in "storeController" to save the favourite marked home.

--- Chapter 16 - Introduction to MongoDB/controllers/storeController.js
+++ Chapter 16 - Introduction to MongoDB/controllers/storeController.js
@@ -43,12 +43,15 @@ exports.getFavouriteList = (req, res, next) => {
 };
 
 exports.postAddToFavourite = (req, res, next) => {
-  Favourite.addToFavourite(req.body.id, error => {
-    if (error) {
-      console.log("Error while marking favourite: ", error);
-    }
-    res.redirect("/favourites");
-  })
+    const homeId = req.body.id;
+    const fav = new Favourite(homeId);
+    fav.save().then(result => {
+        console.log("Fav added", result);
+    }).catch(err => {
+        console.log("Error while marking favourite", err);
+    }).finally(() => {
+        res.redirect("/favourites");
+    });
 }
 
 exports.postRemoveFromFavourite = (req, res, next) => {
--- Chapter 16 - Introduction to MongoDB/models/favourite.js
+++ Chapter 16 - Introduction to MongoDB/models/favourite.js
@@ -1,33 +1,21 @@
-// Core Modules
-const fs = require("fs");
-const path = require("path");
-const rootDir = require("../utils/pathUtil");
-
-const favouriteDataPath = path.join(rootDir, "data", "favourite.json");
+const {getDB} = require("../utils/databaseUtil");
 
 module.exports = class Favourite {
+    constructor(houseId) {
+        this.houseId = houseId;
+    }
+
+    save() {
+        const db = getDB();
+        return db.collection("favourites").insertOne(this);
+    }
 
   static addToFavourite(homeId, callback) {
-    Favourite.getFavourites((favourites) => {
-      if (favourites.includes(homeId)) {
-        callback("Home is already marked favourite");
-      } else {
-        favourites.push(homeId);
-        fs.writeFile(favouriteDataPath, JSON.stringify(favourites), callback);
-      }
-    });
   }
 
   static getFavourites(callback) {
-    fs.readFile(favouriteDataPath, (err, data) => {
-      callback(!err ? JSON.parse(data) : []);
-    });
   }
 
   static deleteById(delHomeId, callback) {
-    Favourite.getFavourites(homeIds => {
-      homeIds = homeIds.filter(homeId => delHomeId !== homeId);
-      fs.writeFile(favouriteDataPath, JSON.stringify(homeIds),callback);
-    })
   }
 };
