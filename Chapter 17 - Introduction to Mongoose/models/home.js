09c20b038ec5a3b5a90d8f940f0232fcdeb4b6b7
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-07-16 15:13:48 +0530

    - Added a home schema "pre" hook to delete the favourite model from DB if the home model is deleted.

--- Chapter 17 - Introduction to Mongoose/models/home.js
+++ Chapter 17 - Introduction to Mongoose/models/home.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const favourite = require("./favourite");
 
 const homeSchema = new mongoose.Schema({
     houseName: {type: String, required: true},
@@ -7,6 +8,12 @@ const homeSchema = new mongoose.Schema({
     rating: {type: Number, required: true},
     photoUrl: String,
     description: String
+});
+
+homeSchema.pre('findOneAndDelete', async function (next) {
+    const homeId = this.getQuery()._id;
+    await favourite.deleteMany({houseId: homeId});
+    // next(); No need to use next() call after Mongoose version 8
 });
 
 module.exports = mongoose.model("Home", homeSchema);
