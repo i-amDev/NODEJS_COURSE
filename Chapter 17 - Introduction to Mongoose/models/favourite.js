5a77d2a2ca10b59a60645208cffe07a16ff64b36
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-07-16 14:48:23 +0530

    - Created "favouriteSchema" using "mongoose" and exported the "favourite" model as well.

--- Chapter 17 - Introduction to Mongoose/models/favourite.js
+++ Chapter 17 - Introduction to Mongoose/models/favourite.js
@@ -1,27 +1,12 @@
-const {ObjectId} = require("mongodb");
+const mongoose = require("mongoose");
 
-module.exports = class Favourite {
-    constructor(houseId) {
-        this.houseId = houseId;
+const favouriteSchema = new mongoose.Schema({
+    houseId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Home",
+        required: true,
+        unique: true
     }
+});
 
-    save() {
-        const db = getDB();
-        return db.collection('favourites').findOne({houseId: this.houseId}).then(existingFav => {
-            if (!existingFav) {
-                return db.collection('favourites').insertOne(this);
-            }
-            return Promise.resolve();
-        });
-    }
-
-  static getFavourites() {
-      const db = getDB();
-      return db.collection("favourites").find().toArray();
-  }
-
-  static deleteById(delHomeId) {
-      const db = getDB();
-      return db.collection("favourites").deleteOne({houseId: new ObjectId(String(delHomeId))});
-  }
-};
+module.exports = mongoose.model("Favourite", favouriteSchema);
