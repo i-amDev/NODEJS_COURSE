0ed8dffe67c330eb72d592421046b67557df21fe
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-07-17 12:28:40 +0530

    - Added a new field "isLoggedIn" in the response.

--- Chapter 18 - Cookies and Sessions/controllers/authController.js
+++ Chapter 18 - Cookies and Sessions/controllers/authController.js
@@ -1,10 +1,13 @@
 exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
     pageTitle: "Login",
-    currentPage: "login"
+    currentPage: "login",
+    isLoggedIn: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
+    console.log(req.body);
+    res.cookie("isLoggedIn", true);
     res.redirect("/");
 };
