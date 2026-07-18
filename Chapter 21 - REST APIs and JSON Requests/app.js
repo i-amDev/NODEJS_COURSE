1b7cdd689d05b7faff789f2c902761710b3ae315
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-07-18 14:06:09 +0530

    - Made a copy of "Chapter 20" to start learning Chapter 21 - REST APIs and JSON Requests.

---
+++ Chapter 21 - REST APIs and JSON Requests/app.js
@@ -0,0 +1,28 @@
+// Core Module
+const path = require('path');
+
+// External Module
+const express = require('express');
+const DB_PATH = "mongodb://127.0.0.1:27017/airbnb";
+
+//Local Module
+const rootDir = require("./utils/pathUtil");
+const errorsController = require("./controllers/errors");
+const { default: mongoose } = require('mongoose');
+
+const app = express();
+
+app.use(express.urlencoded());
+app.use(express.static(path.join(rootDir, 'public')));
+app.use(errorsController.pageNotFound);
+
+const PORT = 3000;
+
+mongoose.connect(DB_PATH).then(() => {
+  console.log('Connected to Mongo');
+  app.listen(PORT, () => {
+    console.log(`Server running on address http://localhost:${PORT}`);
+  });
+}).catch(err => {
+  console.log('Error while connecting to Mongo: ', err);
+});
