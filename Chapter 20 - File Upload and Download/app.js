3a6e04d8b599541ffd8cd15dbc97e9cf70f73db2
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-07-18 12:31:13 +0530

    - Implemented the logic to serve saved data uploaded by the users.

--- Chapter 20 - File Upload and Download/app.js
+++ Chapter 20 - File Upload and Download/app.js
@@ -59,6 +59,8 @@ const multerOptions = {
 app.use(express.urlencoded());
 app.use(multer(multerOptions).single("photoUrl"));
 app.use(express.static(path.join(rootDir, 'public')));
+app.use("/uploads", express.static(path.join(rootDir, 'uploads')));
+app.use("/host/uploads", express.static(path.join(rootDir, 'uploads')));
 
 app.use(session({
   secret: "KnowledgeGate AI with Complete Coding",
